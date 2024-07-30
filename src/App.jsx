b7acbb9f333012f782c9f443df5dec4cbada8b46
Karine Berdigulova <berdigulova9797@gmail.com>
2024-07-29 22:15:59 -0400

    Created Company Card separete reusable component

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import CompanyCard from './components/CompanyCard'
+const company = {
+  name: 'ITF logistics',
+  logo: 'https://marketplace.canva.com/EAF5lVrtgYg/1/0/800w/canva-blue-and-orange-logistics-logo-nWC-8tBqWb8.jpg',
+  website: 'www.itflogistics.com',
+  description: 'We are the best at what we are doing',
+  location: 'Chicago',
+  fleet: '100+ trucs',
+  cargos: ['DryVan', 'Amazon'],
+  phone: '+1 456 544 5544',
+  fax: '+1 456 544 5544',
+  email: 'itf@cargoexample.com',
+  facebookLink: 'https://www.facebook.com/itflogistics',
+  instagramLink: 'https://www.instagram.com/itflogistics',
+}
+
+function App() {
+  return (
+    <div>
+      <CompanyCard company={company} />
+    </div>
+  )
+}
+
+export default App
